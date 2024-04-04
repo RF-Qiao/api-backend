62eaa3c959612fb5967303184016ba421ef76941
qarar <flamaro@163.com>
2024-04-04 10:52:01 +0800

    init code

---
+++ src/main/java/com/qarar/apibackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.qarar.apibackend.exception;
+
+import com.qarar.apibackend.common.BaseResponse;
+import com.qarar.apibackend.common.ErrorCode;
+import com.qarar.apibackend.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
---
+++ src/test/java/com/qarar/apibackend/service/PostServiceTest.java
@@ -0,0 +1,32 @@
+package com.qarar.apibackend.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.qarar.apibackend.model.dto.post.PostQueryRequest;
+import com.qarar.apibackend.model.entity.Post;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+
+/**
+ * 帖子服务测试
+ *
+ */
+@SpringBootTest
+class PostServiceTest {
+
+    @Resource
+    private PostService postService;
+
+    @Test
+    void searchFromEs() {
+        PostQueryRequest postQueryRequest = new PostQueryRequest();
+        postQueryRequest.setUserId(1L);
+        Page<Post> postPage = postService.searchFromEs(postQueryRequest);
+        Assertions.assertNotNull(postPage);
+    }
+
+}
