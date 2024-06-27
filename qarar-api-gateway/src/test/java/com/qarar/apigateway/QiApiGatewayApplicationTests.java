7aaaf530be1a0b0ea137f4ae76abbff06829e92a
乔瑞峰 <1328678738@qq.com>
2024-06-27 17:02:25 +0800

    init

---
+++ qarar-api-gateway/src/test/java/com/qarar/apigateway/QiApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qarar.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
